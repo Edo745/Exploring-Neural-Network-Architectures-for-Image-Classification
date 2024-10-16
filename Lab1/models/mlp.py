af34f2cc67b54692af0172b41f54de32f319c9e5
Edoardo Cappelli <98023490+Edo745@users.noreply.github.com>
2024-10-16 15:16:19 +0200

    Update mlp.py

--- Lab1/models/mlp.py
+++ Lab1/models/mlp.py
@@ -1,23 +1,34 @@
 import torch
-from torch import nn
+import torch.nn as nn
 
 class MLP(nn.Module):
-    def __init__(self, num_classes=10):
+    def __init__(self, input_size=3*32*32, num_classes=10):
         super(MLP, self).__init__()
-         
-        self.mlp = nn.Sequential(
-            nn.Linear(3*32*32, 1024),
+        
+        self.flatten = nn.Flatten()
+        
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
+            
+            nn.Linear(4096, 2048),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            
+            nn.Linear(2048, 1024),
+            nn.ReLU(inplace=True),
+            
             nn.Linear(1024, 512),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.Dropout(0.5),
+            
             nn.Linear(512, 256),
             nn.ReLU(inplace=True),
+            
             nn.Linear(256, num_classes)
         )
 
     def forward(self, x):
-        x = torch.flatten(x, 1)
-        x = self.mlp(x)
+        x = self.flatten(x)
+        x = self.layers(x)
         return x
